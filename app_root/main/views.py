84bf160f2d4d757a54497730c6097dd32f36f625
VolodymyrMamaevsky <vova.moder@gmail.com>
2024-07-18 09:34:38 +0300

    add html templates

--- app_root/main/views.py
+++ app_root/main/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request) -> HttpResponse:
+    context: dict = {"title": "Home", "content": "Store main page"}
+
+    return render(request, "main/index.html", context)
+
+
+def about(request) -> HttpResponse:
+    return HttpResponse("About")
